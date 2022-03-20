8b30318a96ea185c8e226161ee7d92fd52e5f3c9
britbolt <blattisaw@gmail.com>
2022-03-20 16:44:44 -0400

    start timer function added

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # beginner-JS-quiz
+The goal of this page is to provide a short quiz for beginner JS users to test their knowledge. It has 5 basic JS questions and logic to show if the answers are right or wrong, along with a timer for the quiz.
--- assets/quiz.js
+++ assets/quiz.js
@@ -1,23 +1,77 @@
-
-var buttonEl = document.getElementById('btn-start');
-
 //Start screen & button to start quiz
 window.onload = function() {
     document.getElementById('start-page').style.display ='block';
     document.getElementById('end-page').style.display = "none";
-    document.getElementById('questions').style.display = "none"; 
+    document.getElementById('questions').style.display = "none";  
 };
+
 
 //functionality for "start quiz" button
 //btn will change display to first question and begin timer
 
- buttonEl.addEventListener("onClick", startQuiz ()); 
+const buttonEl = document.getElementById('btn-start');
 
-function startQuiz() {
-    document.getElementById('q1').style.display = "block"
-    //q2.style.display = "none";
-//q3.style.display = "none";
-//q4.style.display = "none";
-//q5.style.display = "none";  
+ //define scoreTimer function
+ var time = document.getElementById("clock-score");
+  function scoreTimer() {
+     let time = 60;
+     const score = setInterval(function frame() {
+        if (time == 0) {
+            clearInterval(score);
+            document.getElementById("clock-score").textContent = "TIME'S UP";
+        } else {
+            document.getElementById("clock-score").innerHTML = time + "s";
+            time--;
+        }
+        console.log(time);
+    },1000);
 };
+
+//start timer with button click
+buttonEl.addEventListener("click", function () {
+    document.getElementById('start-page').style.display ='none';
+    document.getElementById('questions').style.display = "block";
+    document.getElementById('q2', 'q3', 'q4', 'q5').style.display = "none";
+    document.getElementById('q1').style.display = "block";
+
+    scoreTimer();
+    });
+
+
+
+    const buttonQ1 = document.getElementById('q1-c4');
+
+    buttonQ1.addEventListener("click", function () {
+        document.getElementById('start-page').style.display ='none';
+        document.getElementById('questions').style.display = "block";
+        document.getElementById('q1', 'q3', 'q4', 'q5').style.display = "none";
+        document.getElementById('q2').style.display = "block";
+    });
+       
+
+
+
+ //function() {
+                //document.getElementById('clock-score').innerHTML = '0' +sec;
+                //sec--;
+                //if (sec < 0) {
+                  //  clearInterval(scoreTimer);
+                //}
+
+   /* var x = setInterval (function() {
+        var scoreTimer = new Date ("0").getTime();
+        var now = new Date("60");
+        var distance = scoreTimer - now;
+        var seconds = Math.floor((distance % (1000*60)) / 1000);
+        document.getElementById("clock-score").textContent = seconds + "s";
+
+    // if timer gets to 0 display Times up message
+    if (distance < 0) {
+            clearInterval(x);
+            
+        } 
+    }, 1000);*/
+
+    
+
 
