46be251b2e31b45c286ff7285bbaff5e11db3eb8
britbolt <blattisaw@gmail.com>
2022-03-20 23:06:37 -0400

    flow through questions and some styling added

--- assets/quiz.js
+++ assets/quiz.js
@@ -1,17 +1,17 @@
 //Start screen & button to start quiz
-window.onload = function() {
-    document.getElementById('start-page').style.display ='block';
-    document.getElementById('end-page').style.display = "none";
-    document.getElementById('questions').style.display = "none";  
-};
+const begin = document.getElementById('start-page');
 
+window.onload = function() {
+    begin.style.display = 'block';
+  };
 
 //functionality for "start quiz" button
 //btn will change display to first question and begin timer
-
 const buttonEl = document.getElementById('btn-start');
+const question1 = document.getElementById('q1');
 
  //define scoreTimer function
+
  var time = document.getElementById("clock-score");
   function scoreTimer() {
      let time = 60;
@@ -19,59 +19,88 @@ const buttonEl = document.getElementById('btn-start');
         if (time == 0) {
             clearInterval(score);
             document.getElementById("clock-score").textContent = "TIME'S UP";
+            document.getElementById('final-score').style.display = "block";
+            document.getElementById('questions', 'start-page').style.display = "none";
         } else {
             document.getElementById("clock-score").innerHTML = time + "s";
             time--;
         }
         console.log(time);
     },1000);
-};
+    };
 
-//start timer with button click
-buttonEl.addEventListener("click", function () {
-    document.getElementById('start-page').style.display ='none';
-    document.getElementById('questions').style.display = "block";
-    document.getElementById('q2', 'q3', 'q4', 'q5').style.display = "none";
-    document.getElementById('q1').style.display = "block";
+    
 
+    //scoreTimer();
+
+
+
+
+//start timer with "start quiz" button click
+//hide start-page div with button click
+buttonEl.addEventListener("click", function showNext() {
+        question1.style.display = 'block';
+        begin.style.display = 'none';
+//call for timer function
     scoreTimer();
     });
 
 
+const question2 = document.getElementById('q2');
+const correctq1 = document.getElementById('correct-q1c4');
 
-    const buttonQ1 = document.getElementById('q1-c4');
-
-    buttonQ1.addEventListener("click", function () {
-        document.getElementById('start-page').style.display ='none';
-        document.getElementById('questions').style.display = "block";
-        document.getElementById('q1', 'q3', 'q4', 'q5').style.display = "none";
-        document.getElementById('q2').style.display = "block";
+//question 1 - correct answer click to go to next question
+    correctq1.addEventListener("click", function showCorrect1() {
+       document.getElementById('right-wrong').innerHTML = 'Correct!';
+       question1.style.display = 'none';
+       question2.style.display = 'block';
     });
-       
+
+    document.querySelectorAll('.incorrect').forEach(incorrect => {
+        incorrect.addEventListener('click', toQuest2 => {
+            document.getElementById('right-wrong').innerHTML = 'Incorrect!';
+            question1.style.display = 'none';
+            question2.style.display = 'block';
+            time-=5;
+            document.getElementById('clock-score').innerHTML = `0${time}`;
+        })
+      });
 
 
+const question3 = document.getElementById('q3');
+const correctq2 = document.getElementById('correct-q2c2');
 
- //function() {
-                //document.getElementById('clock-score').innerHTML = '0' +sec;
-                //sec--;
-                //if (sec < 0) {
-                  //  clearInterval(scoreTimer);
-                //}
+    correctq2.addEventListener("click", function showCorrect2 () {
+        document.getElementById('right-wrong').innerHTML = 'Correct!';
+        question2.style.display = 'none';
+        question3.style.display = 'block';
+    });
 
-   /* var x = setInterval (function() {
-        var scoreTimer = new Date ("0").getTime();
-        var now = new Date("60");
-        var distance = scoreTimer - now;
-        var seconds = Math.floor((distance % (1000*60)) / 1000);
-        document.getElementById("clock-score").textContent = seconds + "s";
-
-    // if timer gets to 0 display Times up message
-    if (distance < 0) {
-            clearInterval(x);
-            
-        } 
-    }, 1000);*/
-
+const question4 = document.getElementById('q4');
+const correctq3 = document.getElementById('correct-q3c3');
     
+    correctq3.addEventListener("click", function showCorrect3 () {
+        document.getElementById('right-wrong').innerHTML = 'Correct!';
+         question3.style.display = 'none';
+        question4.style.display = 'block';
+    });
 
 
+const question5 = document.getElementById('q5');
+const correctq4 = document.getElementById('correct-q4c3');
+        
+    correctq4.addEventListener("click", function showCorrec4 () {
+         document.getElementById('right-wrong').innerHTML = 'Correct!';
+        questio4.style.display = 'none';
+        question5.style.display = 'block';
+    });
+
+const finalScore = document.getElementById('final-score');
+const correctq5 = document.getElementById('correct-q5c1');
+            
+    correctq5.addEventListener("click", function showCorrect5 () {
+        document.getElementById('right-wrong').innerHTML = 'Correct!';
+        question5.style.display = 'none';
+        finalScore.style.display = 'block';
+        clearInterval(score);
+    });
